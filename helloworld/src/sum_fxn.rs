e7772c5976162d9ef61decd2f2a304f62c0521b1
arin-prashar <prashararin@gmail.com>
2024-01-02 21:30:24 +0530

    Add hello_word.rs and sum_fxn.rs files, update main.rs with sum function
    Signed-off-by: arin-prashar <prashararin@gmail.com>

--- helloworld/src/sum_fxn.rs
+++ helloworld/src/sum_fxn.rs
@@ -1,6 +1,10 @@
-fn main(){
+fn main() {
     let p:i32=10;
     let q:i32=20;
-    let res=p+q;
+    let res=sum(p,q);
     println!("Sum of {} and {} is {}",p,q,res);
+}
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
 }
